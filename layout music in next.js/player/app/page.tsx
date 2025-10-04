afd66cc1a43c7fb60a5a959b76af626acd5ebc9b
mmvbs <gtamsue@gmail.com>
2025-10-04 18:29:57 -0300

    versão: final

--- layout music in next.js/player/app/page.tsx
+++ layout music in next.js/player/app/page.tsx
@@ -1,10 +1,6 @@
 "use client";
-import { useRef, useState, useEffect} from "react";
-
+import { useRef, useState, } from "react";
 export default function Home() {
-  const [duration, setDuration] = useState(0);
-  const [currentTime, setCurrentTime] = useState(0);
-  const [progress, setProgress] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
   const audioRef = useRef<HTMLAudioElement | null>(null);
   function togglePlay() {
@@ -17,55 +13,23 @@ export default function Home() {
         audio.pause();
     }
   }
-  useEffect(() => {
-    const audio = audioRef.current;
-    if (!audio) return; 
-
-    const updateProgress = () => {
-        setCurrentTime(audio.currentTime);
-        setProgress((audio.currentTime / audio.duration) * 100 || 0);
-    };
-
-    const handleLoadedMetadata = () => {
-        setDuration(audio.duration);
-    };
-    if (audio.duration > 0) {
-            setDuration(audio.duration);
-        }
-
-    audio.addEventListener("timeupdate", updateProgress);
-    audio.addEventListener("loadedmetadata", handleLoadedMetadata);
-
-    return () => {
-        // Limpa os event listeners quando o componente é desmontado
-        audio.removeEventListener("timeupdate", updateProgress);
-        audio.removeEventListener("loadedmetadata", handleLoadedMetadata);
-    };
-  }, []);
-  const formatTime = (time: number) => {
-    const minutes = Math.floor(time / 60);
-    const seconds = Math.floor(time % 60);
-    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
-  }
   return (
      <div className="bg-[rgb(15,19,22)] flex items-center justify-center h-screen">
-      <div className="bg-[#202830] h-[400px] w-[300px] rounded-[10px] flex flex-col items-center justify-center p-4">
+      <div className="bg-[rgb(32,40,48)] h-[400px] w-[300px] rounded-[10px] flex flex-col items-center justify-center">
         <img className="h-[200] w-[200]" src="a.jpg" alt="" />
         <h1 className="text-white text-[16px] font-bold mt-4">Twin Flame</h1>
-        <h3 className="text-[gray] text-[12px] mt-2">Weyes Blood</h3>
-      <div className="w-[50%]">
-        <div className="w-[100%] h-[4px] bg-[rgb(15,19,22)] rounded-full mt-4">
-         <div className="h-[4px] bg-[#ff302e] rounded-full" style={{ width: `${progress}%` }} ></div>
-        </div>
-        <div className="rounded-full mt-4 flex justify-between text-[12px] text-[gray]">
-        <h3>{formatTime(currentTime)}</h3>
-        <h3>{formatTime(duration)}</h3>
+        <h3 className="text-[gray] text-[12px] font-bold mt-2">Weyes Blood</h3>
+      <div className="w-[50%] mt-4">
+        <div className="w-[100%] h-[4px] bg-[rgb(15,19,22)] rounded-full "></div>
+        <div className="flex items-start justify-between text-[12px] text-[gray]">
+        <h3>0:00</h3>
+        <h3>4:42</h3>
         </div>
       </div>
-      <div className="flex items-center justify-between w-[70%] mt-4">
-        <h3 className="scale-x-[-1]">▶▶</h3>  
+      <div className="flex items-center justify-between w-[50%]">
+        <h3 className="scale-x-[-1] cursor-pointer">▶▶</h3>  
         <button
-        className="text-white text-xl w-[30px] rounded-full h-8 w-8 bg-[#ff302e] flex items-center justify-center"
+        className="text-white w-[24px] h-[24px] rounded-full bg-[#ff302e] flex items-center justify-center cursor-pointer"
         onClick={togglePlay}
         >
         {isPlaying ? "⏸" : "▶"}
@@ -73,7 +37,7 @@ export default function Home() {
         <audio src="WB.mp3" ref={audioRef}>
           <track kind="captions" />
         </audio>
-        <h3>▶▶</h3>
+        <h3 className="cursor-pointer">▶▶</h3>
         <img src="vol.png" alt="volume" className="vol h-[12] w-[12] cursor-pointer" />
       </div>
       </div>
